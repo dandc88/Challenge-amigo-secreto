d9b35d4ab895ba25825b987dd16c9db22768585f
Daniel Dávila <danieldavilavzla@gmail.com>
2025-08-07 09:33:45 -0400

    funcionalidad obtener y mostrar amigos

--- app.js
+++ app.js
@@ -2,8 +2,42 @@
 
 let amigos =[];
 
-let nuevoAmigo = document.getElementById("amigo")
+let contenedorAmigo = document.getElementById('amigo')
+let listaAmigos = document.getElementById('listaAmigos');
 
-function agregarAmigo(nuevoAmigo){
-    console.log(nuevoAmigo)
+
+function agregarAmigo(){
+    //Aqui obtenemos el valor de lo ingresado por el usuario
+    
+    nuevoAmigo = contenedorAmigo.value
+    //Si se ingresa un nombre se anexa a la lista si no se genera la alerta
+    if (nuevoAmigo){
+        amigos.push(nuevoAmigo);
+        contenedorAmigo.value = '';
+
+        // Aqui se llama a la función que muestra la lista en la pagina
+
+        mostrarListaAmigos();   
+    }else{
+        alert("Por favor, inserte un nombre.");
+    }
+    console.log(amigos)
+
+}
+
+function mostrarListaAmigos(){
+    // se limpia la lista de amigos
+    listaAmigos.innerHTML = '';
+
+    //Se recorre el array amigos y se crea una etiqueta por cada uno
+    amigos.forEach(amigo =>{
+        let li = document.createElement('li');
+        li.textContent = amigo;
+
+        // Se agrega la nueva etiqueta a la lista <ul>
+        listaAmigos.appendChild(li);
+    })
+
+
+
 }
